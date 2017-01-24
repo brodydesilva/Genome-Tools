2418c3ceb9adb50ccd2675037f9cbf19b1d104b7
bdesilva <brodydesilva@users.noreply.github.com>
2017-01-23 20:06:42 -0600

    Updated Bash Pipeline for configurability and to stable state

--- bash_pipeline.sh
+++ bash_pipeline.sh
@@ -2,8 +2,19 @@ TARGET_DIR='~/Project2/UCSC/2.1/'
 PREFIX='Clostridium_hathewayi'
 
 TOOLS='/share/apps/ngs-ccts/'
-BEDOPS=$TOOLS'share/apps/ngs-ccts/BEDOPS/bedops_v2.4.19/bin/'
+BEDOPS=$TOOLS'BEDOPS/bedops_v2.4.19/bin/'
 UCSC='/share/apps/ngs-ccts/ucsc-tools/'
+BGZIP='/home/bdesilva/software/tabix-0.2.6/bgzip'
+TABIX='/home/bdesilva/software/tabix-0.2.6/tabix'
+
+FASTA='.fna'
+VCF='.vcf.snp'
+GFF='_concat.gff'
+DICT='.dict'
+TWOBIT='.2bit'
+CS='.chrom.sizes'
+BB='.bb'
+BED='.bed'
 
 # assume all the information is in the target dir
 # assume all files begin with the name of the genome
@@ -14,24 +25,25 @@ UCSC='/share/apps/ngs-ccts/ucsc-tools/'
 #		.vcf
 
 # create .dict file
-java -jar /share/apps/ngs-ccts/picard-tools/picard-tools-1.129/picard.jar CreateSequenceDictionary REFERENCE=$PREFIX'.fa' OUTPUT=$PREFIX'.dict'
+java -jar /share/apps/ngs-ccts/picard-tools/picard-tools-1.129/picard.jar CreateSequenceDictionary REFERENCE=$PREFIX$FASTA OUTPUT=$PREFIX$DICT
 
 # convert fa to bb
-$UCSC'faToTwoBit' $PREFIX'.fa' $PREFIX'.2bit'
+$UCSC'faToTwoBit' $PREFIX$FASTA $PREFIX$TWOBIT
 
 # create .chrom.sizes file
-$UCSC'twoBitInfo' $PREFIX'.2bit' $PREFIX'.chrom.sizes'
+$UCSC'twoBitInfo' $PREFIX$TWOBIT $PREFIX$CS
 
 # convert gff to bed (no filtering or explicit sorting)
-$BEDOPS'convert2bed' -i gff -o bed -d < $PREFIX'.gff' > $PREFIX'.bed'
+$BEDOPS'convert2bed' -i gff -o bed -d < $PREFIX$GFF > $PREFIX$BED
 
 # convert bed to bb
-$UCSC'bedToBigBed' $PREFIX'.bed' $PREFIX'.chrom.sizes' $PREFIX'.bb'
+$UCSC'bedToBigBed' $PREFIX$BED $PREFIX$CS $PREFIX$BB
 
 # gzip and tabix index vcf
-cp $PREFIX'.vcf' $PREFIX'2.vcf'
-bgzip $PREFIX'.vcf'
-mv $PREFIX'2.vcf' $PREFIX'.vcf' # ugly workaround for not knowing how to redirect the bgzip -c to a file instead of redirecting the file itself before bgzip.
+cp $PREFIX$VCF $PREFIX'2'$VCF
+$BGZIP $PREFIX$VCF
+mv $PREFIX'2'$VCF $PREFIX$VCF # ugly workaround for not knowing how to redirect the bgzip -c to a file instead of redirecting the file itself before bgzip.
 
 # tabix index the bgzipped file (for some reason does not modify in place like bgzip)
-tabix -p vcf $PREFIX'.vcf.gz'
+$TABIX -p vcf $PREFIX$VCF'.gz'
+
