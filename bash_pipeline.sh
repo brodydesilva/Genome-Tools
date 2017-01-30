e4c43e7b823225fc132caad8f79877c53656f89f
bdesilva <brodydesilva@users.noreply.github.com>
2017-01-30 16:11:55 -0600

    Generate single vcf files from MS VCF, bgzip and tabix index

--- bash_pipeline.sh
+++ bash_pipeline.sh
@@ -1,7 +1,7 @@
 # Generate data files for UCSC Track Hub from FASTA and GFF files and create custom track files from a VCF file
 # Modify to fit data and tools
-TARGET_DIR='/home/bdesilva/Project2/UCSC/2.1/C_h/'
-PREFIX=$TARGET_DIR'Clostridium_hathewayi'
+TARGET_DIR='/home/bdesilva/Project2/UCSC/2.2/B_v/'
+PREFIX=$TARGET_DIR'Bacteroides_vulgatus'
 TEMP_FILE='_temp'
 
 # Executables for genomics
@@ -10,11 +10,12 @@ BEDOPS=$TOOLS'BEDOPS/bedops_v2.4.19/bin/'
 UCSC=$TOOLS'ucsc-tools/'
 BGZIP='/home/bdesilva/software/tabix-0.2.6/bgzip'
 TABIX='/home/bdesilva/software/tabix-0.2.6/tabix'
+BCFTOOLS='/home/bdesilva/software/bcftools/bin/bcftools'
 
 # File Extensions (must reflect data)
-FASTA='.fna'
+FASTA='.fa'
 VCF='.vcf.snp'
-GFF='_concat.gff'
+GFF='.gff'
 DICT='.dict'
 TWOBIT='.2bit'
 CS='.chrom.sizes'
@@ -58,14 +59,29 @@ awk -v FS='\t' -v OFS='\t' '{if ($4~/gene/) {print $0}}' $PREFIX$BED >> $PREFIX'
 $UCSC'bedToBigBed' -tab $PREFIX'_cds.bed' $PREFIX$CS $PREFIX'_cds.bb'
 $UCSC'bedToBigBed' -tab $PREFIX'_gene.bed' $PREFIX$CS $PREFIX'_gene.bb'
 
+# split and bgzip the individual files --> output as vcf (not bcf)
+# Jorge Amigo https://www.biostars.org/u/375/ for following structure
+SSVCF=$PREFIX$VCF
+for SAMPLE in `$BCFTOOLS view -h $SSVCF | grep "^#CHROM" | cut -f10-`; do
+  $BCFTOOLS view -c1 -Oz -s $SAMPLE -o ${SSVCF/.vcf*/.$SAMPLE'.vcf.gz'} $SSVCF
+done
+
+
+# Create the tabix indexed files
+for SAMPLE in $(find $PREFIX.*.vcf.gz); do
+	$TABIX $SAMPLE;
+done
+
+# add the tracks to the trackDb.txt
+
 # gzip and tabix index vcf
-cp $PREFIX$VCF $PREFIX'2'$VCF
-$BGZIP $PREFIX$VCF
-mv $PREFIX'2'$VCF $PREFIX$VCF # ugly workaround for not knowing how to redirect the bgzip -c to a file instead of redirecting the file itself before bgzip.
+#cp $PREFIX$VCF $PREFIX'2'$VCF
+#$BGZIP $PREFIX$VCF
+#mv $PREFIX'2'$VCF $PREFIX$VCF # ugly workaround for not knowing how to redirect the bgzip -c to a file instead of redirecting the file itself before bgzip.
 
 # tabix index the bgzipped file (for some reason does not modify in place like bgzip)
-$TABIX -p vcf $PREFIX$VCF'.gz'
+#$TABIX -p vcf $PREFIX$VCF'.gz'
 
-cp $PREFIX$VCF $PREFIX'.vcf'
-mv $PREFIX$VCF'.gz' $PREFIX'.vcf.gz'
-mv $PREFIX$VCF'.gz.tbi' $PREFIX'.vcf.gz.tbi'
+#cp $PREFIX$VCF $PREFIX'.vcf'
+#mv $PREFIX$VCF'.gz' $PREFIX'.vcf.gz'
+#mv $PREFIX$VCF'.gz.tbi' $PREFIX'.vcf.gz.tbi'
